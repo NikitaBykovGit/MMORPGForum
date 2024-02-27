3d8f4361bbcb216f72d9dc1b80859a6c0a5e8b3a
Nikita <nikita.bykov1995@gmail.com>
2024-02-27 14:53:32 +0300

    Added post information

--- forum/billboard/views.py
+++ forum/billboard/views.py
@@ -1,10 +1,20 @@
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView, CreateView
 from .models import Post
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'billboard/post.html'
+    context_object_name = 'post'
 
 
 class PostList(ListView):
     model = Post
-    ordering = 'time'
+    ordering = '-time'
     template_name = 'billboard/posts.html'
     context_object_name = 'posts'
     paginate_by = 10
+
+
+class PostCreate(CreateView):
+    pass
