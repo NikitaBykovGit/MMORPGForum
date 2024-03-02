6fb12c47f20ab45de792aad9687cad004e0594f2
Nikita Bykov <nikita.bykov1995@gmail.com>
2024-03-02 22:31:30 +0300

    Added permishions

--- forum/billboard/models.py
+++ forum/billboard/models.py
@@ -51,7 +51,7 @@ class Post(models.Model):
 
 
 class Response(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField(max_length=100)
     status = models.BooleanField(default=False)
@@ -59,13 +59,13 @@ class Response(models.Model):
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['user', 'post'], name='unique response to a post from each user'),
+            models.UniqueConstraint(fields=['author', 'post'], name='unique response to a post from each user'),
         ]
 
     def get_absolute_url(self):
         return reverse('main_page')
 
     def save(self, *args, **kwargs):
-        if self.user == self.post.author:
+        if self.author == self.post.author:
             raise ValidationError('You can not response to your post')
         super().save(*args, **kwargs)
--- forum/billboard/views.py
+++ forum/billboard/views.py
@@ -2,7 +2,7 @@ from abc import ABC
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import OuterRef, Subquery, Count
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseForbidden
 from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
@@ -14,7 +14,15 @@ from .filters import PostFilter, ResponseFilter
 
 class FindResponseMixin(ABC):
     def get_object(self, queryset=None):
-        return Response.objects.get(post_id=self.kwargs['pk'], user_id=self.request.user.id)
+        return Response.objects.get(post_id=self.kwargs['pk'], author=self.request.user)
+
+
+class AuthorRequiredMixin(ABC):
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            return HttpResponseForbidden("Forbidden")
+        return super().dispatch(request, *args, **kwargs)
 
 
 class PostDetail(DetailView):
@@ -32,7 +40,7 @@ class PostList(ListView):
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        response = Response.objects.filter(post_id=OuterRef('pk'), user_id=self.request.user.id)
+        response = Response.objects.filter(post_id=OuterRef('pk'), author_id=self.request.user.id)
         queryset = queryset.annotate(response_exists=Count(Subquery(response.only('id'))))
         self.filterset = PostFilter(self.request.GET, queryset)
         return self.filterset.qs
@@ -62,29 +70,29 @@ class ResponseCreate(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         response = form.save(commit=False)
         response.post_id = self.kwargs['pk']
-        response.user_id = self.request.user.id
+        response.author = self.request.user
         return super().form_valid(form)
 
 
-class PostUpdate(LoginRequiredMixin, UpdateView):
+class PostUpdate(AuthorRequiredMixin, UpdateView):
     form_class = PostForm
     model = Post
     template_name = 'billboard/post_edit.html'
 
 
-class ResponseUpdate(FindResponseMixin, LoginRequiredMixin, UpdateView):
+class ResponseUpdate(FindResponseMixin, AuthorRequiredMixin, UpdateView):
     model = Response
     form_class = ResponseForm
     template_name = 'billboard/post_edit.html'
 
 
-class PostDelete(LoginRequiredMixin, DeleteView):
+class PostDelete(AuthorRequiredMixin, DeleteView):
     model = Post
     template_name = 'billboard/post_delete.html'
     success_url = reverse_lazy('main_page')
 
 
-class ResponseDelete(FindResponseMixin, LoginRequiredMixin, DeleteView):
+class ResponseDelete(FindResponseMixin, AuthorRequiredMixin, DeleteView):
     model = Response
     template_name = 'billboard/post_delete.html'
     success_url = reverse_lazy('main_page')
